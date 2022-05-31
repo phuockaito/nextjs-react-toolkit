16fc1229ebec631f2bce20eea6ccfadc87bf50b3
WE-SOLARBK\phuocth <huuphuocit1999>
2022-05-31 16:42:05 +0700

    fix: comment

--- hooks/use-comment.js
+++ hooks/use-comment.js
@@ -1,16 +1,9 @@
-import * as React from 'react';
 import useSWR from "swr";
 
 export const useComment = (url) => {
     const { data } = useSWR(url);
-    const [dataComment, setDataComment] = React.useState(data);
 
-    React.useEffect(() => {
-        if (data) {
-            setDataComment(data);
-        };
-    }, [data]);
     return {
-        dataComment
+        dataComment: data
     };
 };
