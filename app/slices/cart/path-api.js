9d042525b6821cc03624104eeb143fb474711410
WE-SOLARBK\phuocth <huuphuocit1999>
2022-06-22 13:16:10 +0700

    post cart

--- app/slices/cart/path-api.js
+++ app/slices/cart/path-api.js
@@ -1,4 +1,5 @@
 import { apiCart } from "@/api-client";
+import {createAsyncThunk } from "@reduxjs/toolkit";
 
 export const postCartAPI = createAsyncThunk("postCart", async (data, token) => {
     const response = await apiCart.postCart(data);
